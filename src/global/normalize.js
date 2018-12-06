4ec2f93afd74be6342775be1786d7c41e49ae1ef
SecretBase <whpigpig@gmail.com>
2018-12-06 23:12:50 +0800

    fix: try to fixing global normalize

--- src/global/normalize.js
+++ src/global/normalize.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components"
 import { normalize } from "styled-normalize"
+import { textColorPrimaryOnLight, colorWhite } from "../theme/constants"
 
 export default createGlobalStyle`
   ${normalize}
@@ -7,9 +8,9 @@ export default createGlobalStyle`
     font-size: 1rem;
     line-height: 1.5;
     font-weight: 400;
-    color: ${props => props.theme.textColorPrimaryOnLight};
+    color: ${textColorPrimaryOnLight};
     text-align: left;
-    background-color: ${props => props.theme.colorWhite};
+    background-color: ${colorWhite};
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
       "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
       "Segoe UI Symbol", "Noto Color Emoji";
