94db5e55012b10d0bc67ee986f588167039048b7
SecretBase <5130999+SecretBase@users.noreply.github.com>
2019-05-16 23:50:40 +0800

    docs: upgrade storybook (#5)

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,7 @@ import { withInfo } from "@storybook/addon-info"
 import ThemeDecorator from "./ThemeDecorator"
 
 // automatically import all files ending in *.stories.js
-const req = require.context("../stories", true, /.stories.js$/)
+const req = require.context("../stories", true, /\.stories\.js$/)
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
